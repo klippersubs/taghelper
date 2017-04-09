7f167feff8b8acbd0fa6f8e47f813de0bd9be599
Alice Klipper <alice.klipper@yandex.com>
2017-04-09 14:20:08 +0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,58 @@
+/**
+ * Options for `merge` function.
+ * @see merge
+ */
+export interface IMergeOptions {
+    /**
+     * If `true`, template values will be converted to strings.
+     */
+    stringify? : boolean;
+
+    /**
+     * If `true`, raw values of strings will be used.
+     */
+    raw? : boolean;
+}
+
+/**
+ * Merges template strings and values arrays into one.
+ * @param {string[]} strings — template strings.
+ * @param {Array} rest — template values.
+ * @param {IMergeOptions} [options] — options.
+ * @returns {Array}
+ */
+export function merge (strings : TemplateStringsArray, rest : any[], options : IMergeOptions = {}) : any[] {
+    const buff : any[] = [];
+
+    strings.forEach((item : any, index : number) => {
+        buff.push(options.raw ? item.raw : item);
+
+        if (index < rest.length) {
+            buff.push(options.stringify ? '' + (rest[ index ] as string) : rest[ index ]);
+        }
+    });
+
+    return buff;
+}
+
+/**
+ * Join both template strings and values into one string.
+ * @param {string[]} strings — template strings.
+ * @param {Array} rest — template values.
+ * @param {IMergeOptions} [options] — options for `merge` function. NB: `stringify` option is useless here.
+ * @returns {string}
+ * @see merge
+ */
+export function join (strings : TemplateStringsArray, rest : any[], options : IMergeOptions = {}) : string {
+    return merge(strings, rest, options).join('');
+}
+
+/**
+ * Return `true`, if it looks like supplied parameters are template strings and values arrays.
+ * @param {string[]} strings — template strings.
+ * @param {Array} rest — template values.
+ * @returns {boolean}
+ */
+export function taglike (strings : TemplateStringsArray | any, rest : any[]) : boolean {
+    return Array.isArray(strings) && Array.isArray(rest);
+}
